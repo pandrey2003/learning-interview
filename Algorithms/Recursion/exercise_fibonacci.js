6bd5bbe20d09dc66a14c93df3c26b6d607915a8a
Andrew Polukhin <qq7039108@gmail.com>
2021-05-12 10:45:06 +0300

    Recursion: fibonacci exercise

---
+++ Algorithms/Recursion/exercise_fibonacci.js
@@ -0,0 +1,12 @@
+function fibonacci(n) {
+    // 0, 1, 1, 2, 3, 5,...
+    if (n === 0) {
+        return 0;
+    }
+    if (n === 1) {
+        return 1;
+    }
+    return fibonacci(n-1)+fibonacci(n-2);
+}
+
+console.log(fibonacci(5));
