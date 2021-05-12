03d92a4d9ba44dbdaac14179476a153974ec9e55
Andrew Polukhin <qq7039108@gmail.com>
2021-05-12 10:58:46 +0300

    Recursion: exercise fibonacci (iterative)

--- Algorithms/Recursion/exercise_fibonacci.js
+++ Algorithms/Recursion/exercise_fibonacci.js
@@ -1,4 +1,4 @@
-function fibonacci(n) {
+function fibonacciRecursive(n) {
     // 0, 1, 1, 2, 3, 5,...
     if (n === 0) {
         return 0;
@@ -6,7 +6,19 @@ function fibonacci(n) {
     if (n === 1) {
         return 1;
     }
-    return fibonacci(n-1)+fibonacci(n-2);
+    return fibonacciRecursive(n-1)+fibonacciRecursive(n-2);
 }
 
-console.log(fibonacci(5));
+function fibonacciIterative(n) {
+    // 0, 1, 1, 2, 3, 5,...
+    let arr = [0, 1];
+    for (let i = 2; i <=n; i++) {
+        arr.push(arr[i-1] + arr[i-2]);
+    }
+    return arr[n];
+}
+
+if (require.main === module) {
+    console.log(fibonacciRecursive(8));
+    console.log(fibonacciIterative(8));
+}
