e5da0f8d1351ab93acb494993e75efbeee3e70d7
Andrew Polukhin <qq7039108@gmail.com>
2021-06-10 10:42:29 +0300

    Exercise: paint house

---
+++ SolvingProblems/paint_house.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/paint-house/
+
+
+class Solution:
+    """
+    @param costs: n x 3 cost matrix
+    @return: An integer, the minimum cost to paint all houses
+    """
+    def minCost(self, costs):
+        if costs is None or len(costs) == 0:
+            return 0
+        for i in range(1, len(costs)):
+            costs[i][0] += min(costs[i-1][1], costs[i-1][2])
+            costs[i][1] += min(costs[i-1][0], costs[i-1][2])
+            costs[i][2] += min(costs[i-1][0], costs[i-1][1])
+        return min(costs[-1])
