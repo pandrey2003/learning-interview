810b04b7f49e9b83b5ff51e35392927c3d24895f
Andrew Polukhin <qq7039108@gmail.com>
2021-06-10 21:07:53 +0300

    Exercise: missing number (extra solution)

--- SolvingProblems/missing_number.py
+++ SolvingProblems/missing_number.py
@@ -5,3 +5,9 @@ def missing_number(nums: list[int]) -> int:
     n = len(nums)
     set_missing_elem = set(range(n + 1)) - set(nums)
     return next(iter(set_missing_elem))
+
+
+def missing_number_alternative(nums: list[int]) -> int:
+    received_sum = sum(nums)
+    needed_sum = len(nums) * (len(nums) + 1) // 2
+    return needed_sum - received_sum
