4af50812bdabb728648413c6e0e1dbc8092c9001
Andrew Polukhin <qq7039108@gmail.com>
2021-06-13 21:54:46 +0300

    Exercise: letter combinations (itertools)

--- SolvingProblems/letter_combinations.py
+++ SolvingProblems/letter_combinations.py
@@ -19,5 +19,18 @@ def letter_combinations(digits: str) -> list[str]:
     return answer
 
 
+def letter_combinations_itertools(digits: str) -> list[str]:
+    if digits == "":
+        return []
+    all_iterables = []
+    for digit in digits:
+        all_iterables.append(mapping[digit])
+    from itertools import product
+    combinations_as_tuples = list(product(*all_iterables))
+    return ["".join(w) for w in combinations_as_tuples]
+
+
 if __name__ == "__main__":
     print(letter_combinations("23"))
+    print(letter_combinations_itertools("23"))
+    print(letter_combinations_itertools("469"))
