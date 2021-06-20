8fa16b2e7f90c7d9fa0242d61ee5875f6a05021a
Andrew Polukhin <qq7039108@gmail.com>
2021-06-20 15:35:47 +0300

    Exercise: unique paths (dynamic programming)

--- SolvingProblems/unique_paths.py
+++ SolvingProblems/unique_paths.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/problems/unique-paths/
 class Solution:
     # Recursive solution giving "Time Limit Exceeded"
     def uniquePaths(self, m: int, n: int) -> int:
@@ -8,3 +9,15 @@ class Solution:
             return 0
         return 1 + self.uniquePathsRecursive(m, n, curr_m + 1, curr_n) \
             + self.uniquePathsRecursive(m, n, curr_m, curr_n + 1)
+
+
+def unique_paths_dp(m: int, n: int) -> int:
+    dp = [[0] * m] * n
+    for i in range(len(dp)):
+        dp[i][0] = 1
+    for i in range(1, len(dp[0])):
+        dp[0][i] = 1
+    for i in range(1, len(dp)):
+        for j in range(1, len(dp[i])):
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+    return dp[-1][-1]
